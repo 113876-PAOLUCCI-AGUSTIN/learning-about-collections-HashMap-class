da4c30e60c91e0f85e3b710a3f36391cd85f9c7e
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-04-21 12:05:49 -0300

    Add info & comments.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
+/*
+ * https://docs.oracle.com/javase/8/docs/api/java/util/HashMap.
+ */
+
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
+
+        // CREATING A HASHMAP
         HashMap<String, String> phoneBook = new HashMap<>();
+
+        // ADDING ELEMENTS IN AN ARRAY.
         phoneBook.put("Agustin", "+54-351-6133688");
         phoneBook.put("Carlos Horacio","+54-3541-502035");
         phoneBook.put("Maria Victoria", "+54-3541-162852");
 
+        // GETTING THE ELEMENTS.
         for (String key : phoneBook.keySet()) {
             System.out.println(key + " : " + phoneBook.get(key));
         }
